8c7ff7ee96a5460f9f40139dc45857066efc9de0
Bioniko <85115922+Bioniko@users.noreply.github.com>
2025-07-29 13:54:43 -0600

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,10 +1,12 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 require __DIR__ . '/vendor/autoload.php';
 
 use SendGrid\Mail\Mail;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre  = htmlspecialchars($_POST['nombre'] ?? '');
